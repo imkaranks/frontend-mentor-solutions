2fc7d30b4dc5b02ebd8d3c7bfeab27ad90534527
Karan <karankumar2004.kk@gmail.com>
2023-04-09 15:09:11 +0530

    feat: allow tabing through planet's articles

--- space-tourism/js/index.js
+++ space-tourism/js/index.js
@@ -33,3 +33,44 @@ function handleActiveState(elements) {
 }
 
 handleActiveState($tabBtns);
+
+window.addEventListener("load", async () => {
+  try {
+    const response = await fetch("./js/data.json");
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const { destinations, crew, technology } = await response.json();
+    
+    if (document.body.classList.contains("bg-layout--destination")) {
+      $tabBtns.forEach(btn => {
+        btn.addEventListener("click", event => {
+          loadDestinationCard(event.target.dataset.controls, destinations);
+        });
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+function loadDestinationCard (planet, data) {
+  const [ destination ] = data.filter(item => item.name === planet);
+
+  document.querySelector('.destination__image').src = destination.images.webp;
+  document.querySelector('.destination__image').alt = destination.name;
+  document.querySelector('.destination__info').innerHTML = `
+    <h3 class="destination__title text-800 font-accent text-white uppercase">${destination.name}</h3>
+    <p>${destination.description}</p>
+    <div class="destination__stats grid uppercase">
+        <div class="destination__stat">
+            <h4 class="text-300 font-primary-cond letter-space-400">Avg. distance</h4>
+            <p class="text-500 text-white">${destination.distance}</p>
+        </div>
+        <div class="destination__stat">
+            <h4 class="text-300 font-primary-cond letter-space-400">Est. travel time</h4>
+            <p class="text-500 text-white">${destination.travel}</p>
+        </div>
+    </div>
+    `;
+}
