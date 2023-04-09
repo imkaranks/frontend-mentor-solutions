c9c1402afbe5c5505121bc98967c40b926e2f0b0
Karan <karankumar2004.kk@gmail.com>
2023-04-09 17:59:02 +0530

    feat: load activity record
    Load activity record based on different periodicity and
    handle their active state for visual contrast.

--- time-tracking-dashboard/js/index.js
+++ time-tracking-dashboard/js/index.js
@@ -1,20 +1,35 @@
 const $scheduleWrapper = document.querySelector(".dashboard__schedule");
+const $btns = document.querySelectorAll(".btn");
 
 window.addEventListener("load", async () => {
   const response = await fetch("./js/data.json");
   const data = await response.json();
   loadScheduleCard(data, "daily");
+
+  $btns.forEach(btn => {
+    btn.addEventListener('click', event => {
+      for (let $btn of $btns) {
+        $btn.setAttribute("data-active", false);
+      }
+      btn.setAttribute("data-active", true);
+
+      const time = event.target.dataset.controls;
+
+      loadScheduleCard(data, time);
+    })
+  })
 })
 
-function loadScheduleCard (data, time) {
-  data.forEach(item => {
+function loadScheduleCard (schedules, time) {
+  $scheduleWrapper.innerHTML = "";
+  schedules.forEach(schedule => {
     $scheduleWrapper.innerHTML += 
-      `<div class="card rounded-md bg-${item.title.toLowerCase()}">
-        <img src="./images/icon-${item.title.toLowerCase().replace(' ', '-')}.svg" alt="">
+      `<div class="card rounded-md bg-${schedule.title.toLowerCase()}">
+        <img src="./images/icon-${schedule.title.toLowerCase().replace(' ', '-')}.svg" alt="">
         <div class="card__content rounded-md">
-            <h2 class="card__title">${item.title}</h2>
-            <p class="card__subtitle--big">${item.timeframes[time].current}</p>
-            <p class="">Last week - <span>${item.timeframes[time].previous}</span></p>
+          <h2 class="card__title">${schedule.title}</h2>
+          <p class="card__subtitle--big">${schedule.timeframes[time].current}</p>
+          <p class="">Last week - <span>${schedule.timeframes[time].previous}</span></p>
         </div>
       </div>`;
   });
